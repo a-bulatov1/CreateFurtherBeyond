75a3bc00ae28fd6e16950086f73a5da5c8c44572
a-bulatov1 <abulatov1423@gmail.com>
2026-06-12 11:00:13 -0700

    Removed steel, nuclear

---
+++ kubejs/server_scripts/removedItems.js
@@ -0,0 +1,42 @@
+const removedItems = [
+    // oritech
+    'oritech:biosteel_ingot',
+    'oritech:biosteel_block',
+    'oritech:biosteel_dust',
+    'oritech:steel_ingot',
+    'oritech:steel_dust',
+    'oritech:steel_block',
+
+    // stellaris
+    'stellaris:steel_ingot',
+    'stellaris:steel_nugget',
+    'stellaris:steel_block',
+    'stellaris:raw_steel_block',
+    'stellaris:raw_steel_ingot',
+    'stellaris:steel_ore',
+    'stellaris:deepslate_steel_ore',
+    'stellaris:raw_uranium',
+    'stellaris:raw_uranium_block',
+    'stellaris:uranium_ingot',
+    'stellaris:uranium_block',
+    'stellaris:mercury_uranium_ore'
+    
+];
+
+const removedRecipes = [
+    {id: 'oritech:foundry/alloy/biosteel'}
+];
+
+RecipeViewerEvents.removeEntriesCompletely('item', event => {
+    event.remove(removedItems)     
+})
+
+ServerEvents.recipes(event => {
+    // quick remove for the non-special recipes (non-oritech mostly)
+    removedItems.forEach(item => event.remove({output: item}))
+    //for (let i = 0; i < removedItems.length; i++) {
+    //    event.remove({output: removedItems[i]})    
+    //}
+    // one off recipes that arent deleted normally
+    event.remove(removedRecipes)
+})
